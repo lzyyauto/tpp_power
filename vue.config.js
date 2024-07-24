54b1a35c8cd447374e3d2b394fe769d2c845a68d
lzyyauto <lzyyauto@gmail.com>
2024-07-24 17:36:34 +0800

    增加检修页

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  devServer: {
+    setupMiddlewares: (middlewares, devServer) => {
+      const MAINTENANCE_MODE = process.env.VUE_APP_MAINTENANCE_MODE === 'true';
+
+      if (MAINTENANCE_MODE) {
+        devServer.app.get('*', (req, res) => {
+          res.sendFile(path.resolve(__dirname, 'public', 'maintenance.html'));
+        });
+      }
+
+      return middlewares;
+    }
+  }
+});
